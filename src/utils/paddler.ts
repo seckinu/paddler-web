1f9b5efba526a07e0bdb3a57ff91cf410d0a700d
seckinu <seckinerenuzun@gmail.com>
2026-06-09 18:19:29 +0300

    allow dict selection

--- src/utils/paddler.ts
+++ src/utils/paddler.ts
@@ -25,9 +25,14 @@ export const FEATURES = [
 	"highregister",
 ] as const;
 
-export async function get_matches(input: string): Promise<string[]> {
+export const DICTIONARIES = ["en_US", "en_CMUDICT"] as const;
+
+export async function get_matches(
+	input: string,
+	dict: (typeof DICTIONARIES)[number] = DICTIONARIES[0],
+): Promise<string[]> {
 	const { stdout } = Deno.spawn("./engine", {
-		args: [input.trim(), "--dict=en.txt"],
+		args: [input.trim(), `--dict=./dictionaries/${dict}.txt`],
 		stdout: "piped",
 	});
 
