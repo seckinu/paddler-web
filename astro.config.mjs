e095469c0e5003a972dc25a9ae3554fcfca70106
seckinu <seckinerenuzun@gmail.com>
2026-06-09 17:39:06 +0300

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+
+import deno from "@deno/astro-adapter";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	output: "server",
+	adapter: deno(),
+});
---
+++ src/utils/paddler.ts
@@ -0,0 +1,37 @@
+export const FEATURES = [
+	"syllabic",
+	"sonorant",
+	"consonantal",
+	"continuant",
+	"delayedrelease",
+	"lateral",
+	"nasal",
+	"strident",
+	"voice",
+	"spreadglottis",
+	"constrictedglottis",
+	"anterior",
+	"coronal",
+	"distributed",
+	"labial",
+	"high",
+	"low",
+	"back",
+	"round",
+	"velaric",
+	"tense",
+	"long",
+	"hightone",
+	"highregister",
+] as const;
+
+export async function get_matches(input: string): Promise<string[]> {
+	const { stdout } = Deno.spawn("./engine", {
+		args: [input, "--dict=en.txt"],
+		stdout: "piped",
+	});
+
+	const output = (await stdout.text()).trim();
+
+	return output.split("\n");
+}
